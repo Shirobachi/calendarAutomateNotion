42336c225e025455be66cd68f1b4d65098d15afe
Shirobachi <simon@hryszko.dev>
2021-02-28 12:17:38 +0100

    I made full automatin
    - comunication to calendar API
    - comunication to notion API
    - comunication to discord webhook
    - logic what copy next day events to notion and send as message to
    discord

--- cal.py
+++ cal.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from http.client import parse_headers
 from notion.block import PageBlock
 from notion.block import TodoBlock
 from notion.client import NotionClient
@@ -12,6 +13,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import logging
 from dotenv import load_dotenv
+from notion.collection import NotionDate
 
 
 def init():
@@ -25,16 +27,17 @@ def init():
 def getVariable(key):
     load_dotenv()
     return os.getenv(key)
-
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    logging.info("Loaded local varirabled")
 
 
 def _getGoodCreds():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
+
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -53,6 +56,7 @@ def _getGoodCreds():
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    logging.info("Creds succefully get")
     return creds
 
 
@@ -63,8 +67,10 @@ def getEvents(limit=1000):
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    events_result = service.events().list(maxResults=limit, calendarId='54g7n4diedrddf8cdm11sl50mk5fj3r3@import.calendar.google.com', timeMin=now, singleEvents=True, orderBy='startTime').execute()
+    # events_result = service.events().list(maxResults=limit, calendarId='54g7n4diedrddf8cdm11sl50mk5fj3r3@import.calendar.google.com', timeMin=now, singleEvents=True, orderBy='startTime').execute()
+    events_result = service.events().list(maxResults=limit, calendarId='primary', timeMin=now, singleEvents=True, orderBy='startTime').execute()
     events = events_result.get('items', [])
+    logging.info("Get events from google API")
 
     return events
 
@@ -74,61 +80,75 @@ def sendToDiscord(text):
 
     webhook = DiscordWebhook(url=_url, content=text)
     response = webhook.execute()
+    logging.info("Sent message by discord webhook")
+
+
+def _stringToDate(data):
+
+    dt = dateutil.parser.parse(data)
+    str = dt.strftime("%Y-%m-%d %H:%M")
+    result = datetime.datetime.strptime(str, "%Y-%m-%d %H:%M")
+
+    return result
 
 
 def callNotionAPI():
-    token = getVariable(NOTION_TOKEN)
-    db_url = "https://www.notion.so/shirobachi/020cfa41c8254c9196deb6e93d6f5ce0?v=5b4c4ff4e74e4614b1ea9d334204930a"
-    pg_url = "https://www.notion.so/shirobachi/test-96d3090d48f746b98b36f6c2b0286d40"
+    token = getVariable("NOTION_TOKEN")
 
-    client = NotionClient(token_v2=token)
-    db = client.get_collection_view(db_url)
-
-    # row = db.collection.add_row()
-
-    # events = getEvents(1)
-    # time = events[0]['start'].get('dateTime')
-    # dt = dateutil.parser.parse(time)
-    # tmp = dt.strftime('%b %e, %Y')
-    # row.Date = tmp
-    # print(tmp)
-
-    rows = db.collection.get_rows()
-    for row in rows:
-        print("created" + str(row.created))
-        print("Date" + str(row.Date))
-        print("date" + str(row.date))
-        print("a" + str(row.a))
+    return NotionClient(token_v2=token)
 
 
-print(getVariable("DISORD_WEBHOOK"))
+def parseName(input):
+    output = "["
+    temp = input.split(" - ")
+    output += temp[0]
+    output += "] "
 
-events = getEvents(12)
+    temp2 = temp[1].split(" ")
+    for word in temp2:
+        if len(word) > 1:
+            output += word[0].upper()
+        else:
+            output += word
+    logging.info("Parsed name")
+    return output
 
-text = ""
+
+init()
+client = callNotionAPI()
+
+db_url = "https://www.notion.so/shirobachi/0c79483ecf20424aa5a97781741ec185?v=ffa16d64036846c8b39eb9ac5300381c"
+db = client.get_collection_view(db_url)
+
+events = getEvents()
+
+today = datetime.datetime.now().strftime("%d%m%Y")
+today = datetime.datetime.strptime(today, "%d%m%Y")
+today += datetime.timedelta(days=1)
+today = today.strftime("%d%m%Y")
+
+message = "Jutrzejsze zajęcia:\n"
 
 for event in events:
-    text += "- " + str(event['summary']) + "\n"
+    dt = dateutil.parser.parse(event['start']['dateTime'])
+    eventStamp = dt.strftime("%d%m%Y")
 
-text = "Lista zajęć:\n"+text
-print(text)
-sendToDiscord(text)
+    if today == eventStamp:
 
+        start = _stringToDate(event['start']['dateTime'])
+        end = _stringToDate(event['end']['dateTime'])
+        startHour = dt.strftime("%H:%M")
 
-# callNotionAPI()
+        row = db.collection.add_row()
+        row.title = today
+        row.title = parseName(event['summary'])
+        row.date = NotionDate(start, end=end)
+        row.tags = "College"
+        message += "- " + startHour + ": " + event['summary']
+        if parseName(event['summary']) == "[WKLD] GK":
+            message += " [ https://join.skype.com/aUFNC3LWFlpo ]"
+        message += "\n"
+    else:
+        break
 
-# events = getEvents(1)
-# messageSufix = events[0]['summary']
-
-# sendToDiscord("Next class is " + messageSufix)
-
-
-# if not events:
-#     print('No upcoming events found.')
-# for event in events:
-#     start = event['start'].get('dateTime', event['start'].get('date'))
-
-#     dt = dateutil.parser.parse(event['start'].get('dateTime'))
-#     tmp = dt.strftime('%H:%M')
-#     print(tmp, end="")
-#     print(event['summary'])
+sendToDiscord(message)
